1b946c5749c550e618b8c6e622864cb56ef291e7
Plimon Howard V <plimon.howard@gmail.com>
2020-08-06 21:02:05 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,8 @@
 def snake_it_up(string)
-#  if string[0] == "s"
-#    9.times do
-  #   string = string.prepend("s")
-#  else
-  #  return string
-  #binding.pry
-#  return string
+  if string[0] == "s"
+    9.times do 
+      string = string.prepend("s")
+  else
+    return string
+  end
 end
